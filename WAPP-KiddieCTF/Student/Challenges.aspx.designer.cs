ea8bb85d44c9b78af0b6c8a8e3f4bc088f738811
huinannn <huinan26@gmail.com>
2025-11-06 07:54:38 +0800

    student challenges

--- WAPP-KiddieCTF/Student/Challenges.aspx.designer.cs
+++ WAPP-KiddieCTF/Student/Challenges.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace WAPP_KiddieCTF.Student
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::WAPP_KiddieCTF.Student.SideBar SidebarControl;
+
+        /// <summary>
+        /// categoryTabs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder categoryTabs;
+
+        /// <summary>
+        /// challengeGrid control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel challengeGrid;
     }
 }
